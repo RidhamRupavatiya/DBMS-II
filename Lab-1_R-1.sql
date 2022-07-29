4dd80a4db1a0954954726caca7bd23058d8fcae8
RidhamRupavatiya <108151918+RidhamRupavatiya@users.noreply.github.com>
2022-07-30 00:53:46 +0530

    Update Lab-1_R-1.sql

--- Lab-1_R-1.sql
+++ Lab-1_R-1.sql
@@ -17,24 +17,24 @@ insert into Student values(105 , 'Vimal Trivedi' , '090200107103' , 'BCY-3' ,  3
 
 ------Queries------
 
-select Name from Student where Sem in(3 , 5);									--1st Query
+select Name from Student where Sem in(3 , 5);							 --1st Query
 select Name,Enrollment from Student where StuID>103 and StuID<105;				 --2nd Query
-select Name,Enrollment,Email from Student where Sem = 5;						--3rd Query
-select top 3 *from Student														--4th Query
-select top 30 percent Name,Enrollment from Student where ContactNo like '%7';   --5th Query
-select distinct Sem from Student												--6th Query
-select *from Student where enrollment is null;									--7th Query
-select *from Student where name not like 'v%';									--8th Query
-select *from Student where Email like '%3@G%' and  email like '______';			--9th Query
-select *from Student where name like 'F_R%';									--10th Query
-select *from Student where ContactNo like '%877%';								--11th Query
+select Name,Enrollment,Email from Student where Sem = 5;				         --3rd Query
+select top 3 *from Student								         --4th Query
+select top 30 percent Name,Enrollment from Student where ContactNo like '%7';   		--5th Query
+select distinct Sem from Student								--6th Query
+select *from Student where enrollment is null;							--7th Query
+select *from Student where name not like 'v%';							--8th Query
+select *from Student where Email like '%3@G%' and  email like '______';				--9th Query
+select *from Student where name like 'F_R%';							--10th Query
+select *from Student where ContactNo like '%877%';						--11th Query
 select Name from Student where sem = 3 and ContactNo not like '%[8,9]%';			--12th Query
-select Name from Student where BirthDate>'01-01-1990';							--13th Query
+select Name from Student where BirthDate>'01-01-1990';						--13th Query
 update student set Division = 'BCX-5' , sem = 5 where StuId=102;				--14th Query
-update student set name = 'Firoz Sherasiya' where email ='firox.me@gmail.com';	--15th Query
-alter table Student add city varchar(50);										--16th Query
-delete from Student where Division='BCX-3';										--17th Query
-sp_rename 'Student.Email' , 'EmailAddress';										--18th Query
+update student set name = 'Firoz Sherasiya' where email ='firox.me@gmail.com';			--15th Query
+alter table Student add city varchar(50);							--16th Query
+delete from Student where Division='BCX-3';							--17th Query
+sp_rename 'Student.Email' , 'EmailAddress';							--18th Query
 
 --Query -19
 select * into Student_New from Student;
